8bcb31578df8e43aec0061128296ec73e9edf824
Aomeli <43156436+Aomeli@users.noreply.github.com>
2019-04-25 23:17:47 -0500

    Add files via upload

---
+++ elementos/src/main/java/org/unitec/elementos/Localizacion.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.elementos;
+
+/**
+ *
+ * @author jdnsp
+ */
+public class Localizacion {
+    double lat;
+    double lon;
+
+     public Localizacion() {
+        
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+     
+  
+
+   
+    
+    
+    
+    
+}
